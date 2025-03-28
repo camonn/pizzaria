deaed915e3c4397cb1cb445a52d548ce1431a0e4
camonn <camonnritse18@gmail.com>
2025-03-27 23:01:25 -0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// Menu hambúrguer
+// Script para o menu hambúrguer
 const menuToggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementById("nav-links");
 
+// Função que alterna a classe 'active' no menu
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
